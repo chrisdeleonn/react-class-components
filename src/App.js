9f3ee0f242aacccc14b8be41c9b993dc47068174
chrisdeleonn <cdizzel@ymail.com>
2021-07-29 17:02:08 -0400

    class components vs func components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import HomeClass from './components/HomeClass'
+import HomeFunc from './components/HomeFunc'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <p>class:</p>
+      <HomeClass />
+      <p>func:</p>
+      <HomeFunc />
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/HomeFunc.js
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function HomeFunc() {
+  const [counter, setCounter] = useState(0)
+  return (
+    <div style={{ padding: '20px' }}>
+      <h2>Counter: {counter}</h2>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+      &nbsp;
+      <button onClick={() => setCounter(counter - 1)}>-</button>
+    </div>
+  )
+}
+
+export default HomeFunc
